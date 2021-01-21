8e51f289470f9ddd72cadb235f5253b13987e985
Vladimir Lugom <www.lugom96@mail.ru>
2021-01-22 01:40:26 +0300

    first commit

--- src/main/java/ru/lugom/controller/FilmController.java
+++ src/main/java/ru/lugom/controller/FilmController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import ru.lugom.model.Film;
 import ru.lugom.service.FilmService;
-import ru.lugom.service.FilmServiceImpl;
 
 @Controller
 public class FilmController {
--- src/main/java/ru/lugom/test.java
+++
@@ -1,9 +0,0 @@
-package ru.lugom;
-
-public class test {
-    public static void main(String[] args) {
-        String s1 = "unhappy";
-        System.out.println(s1.substring(0,2));
-    }
-
-}
